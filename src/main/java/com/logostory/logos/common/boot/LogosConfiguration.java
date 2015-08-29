c50f494bac4f1dcb8efef392e5c0e3ba50c7067f
david.sun <sunys99@gmail.com>
2015-08-29 14:04:31 +0900

    update for jsonp sample

--- src/main/java/com/logostory/logos/common/boot/LogosConfiguration.java
+++ src/main/java/com/logostory/logos/common/boot/LogosConfiguration.java
@@ -1,12 +1,32 @@
 package com.logostory.logos.common.boot;
 
+import java.util.Collections;
+
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 
 @Configuration
 @ComponentScan("com.logostory.logos")
 @ImportResource("classpath*:spring/context-*.xml")
 public class LogosConfiguration {
+	
+	@ControllerAdvice
+    static class JsonpAdvice extends AbstractJsonpResponseBodyAdvice {
+        public JsonpAdvice() {
+            super("callback");
+        }
+    }
+
+    @Bean
+    public HttpMessageConverters customConverters() {
+        return new HttpMessageConverters(false, Collections.<HttpMessageConverter<?> >singleton(new MappingJackson2HttpMessageConverter()));
+    }
 
 }
